2d123d3a6a45d9db9686d6240a5211f3aa3be7ce
fmerino-indra <felix.merino@gmail.com>
2023-02-22 10:31:32 +0100

    Second commit

---
+++ register-server/src/main/java/fmm/cloud/demo/registerserver/RegisterServerApplication.java
@@ -0,0 +1,13 @@
+package fmm.cloud.demo.registerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServerApplication.class, args);
+	}
+
+}
